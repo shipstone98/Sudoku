aaee032b5c0cc5abc75f38097d28d54814d20613
Christopher Shipstone <christopher.shipstone98@gmail.com>
2020-07-26 00:48:48 +0100

    Added basic structure for moves

---
+++ Sudoku/SudokuMove.cs
@@ -0,0 +1,74 @@
+using System;
+
+namespace Sudoku
+{
+    public class SudokuMove
+    {
+        public const int DefaultPatternNamePadding = 16;
+
+        private readonly int[] _Possible;
+
+        public int Column { get; }
+        public int Number { get; }
+        public SudokuPattern Pattern { get; }
+
+        public String PatternName
+        {
+            get
+            {
+                switch (this.Pattern)
+                {
+                    case SudokuPattern.NakedSingle:
+                        return "Naked Single";
+                    default:
+                        return String.Empty;
+                }
+            }
+        }
+
+        public int[] Possible => this._Possible;
+        public int Row { get; }
+
+        public SudokuMove(int row, int column, int number, SudokuPattern pattern, int[] possible)
+        {
+            if (row < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof (row));
+            }
+
+            if (column < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof (column));
+            }
+
+            if (number < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof (number));
+            }
+
+            if (pattern == SudokuPattern.None)
+            {
+                throw new ArgumentException(nameof (pattern));
+            }
+
+            if (possible is null)
+            {
+                throw new ArgumentNullException(nameof (possible));
+            }
+
+            if (possible.Length == 0)
+            {
+                throw new ArgumentException(nameof (possible));
+            }
+            
+            this._Possible = possible;
+            this.Column = column;
+            this.Number = number;
+            this.Pattern = pattern;
+            this.Row = row;
+        }
+
+        public override String ToString() => this.ToString(SudokuMove.DefaultPatternNamePadding);
+        public virtual String ToString(int patternNamePadding) => String.Join("\t", this.Row, this.Column, this.Number, this.PatternName.PadRight(patternNamePadding), String.Join<int>(",", this.Possible));
+    }
+}
