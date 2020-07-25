e8a4f01aabbca62e9b469f78637f0816d06c4d1d
Christopher Shipstone <christopher.shipstone98@gmail.com>
2020-07-25 21:35:45 +0100

    Added number filler

---
+++ Sudoku.Test/SudokuGeneratorTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Sudoku.Test
+{
+	[TestClass]
+	public class SudokuGeneratorTest
+	{
+		[TestMethod]
+		public void TestAddNumbers()
+		{
+			Sudoku sudoku = SudokuGenerator.AddNumbers(SudokuTest.Size, SudokuTest.Difficulty);
+
+			for (int i = 0; i < sudoku.Size; i ++)
+			{
+				for (int j = 0; j < sudoku.Size; j ++)
+				{
+					int number = sudoku[i, j];
+					Assert.IsFalse(sudoku.RowContains(i, j, number, true));
+					Assert.IsFalse(sudoku.ColumnContains(i, j, number, true));
+					Assert.IsFalse(sudoku.BlockContains(i, j, number, true));
+				}
+			}
+		}
+	}
+}
---
+++ Sudoku/SudokuGenerator.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Sudoku
+{
+	public static class SudokuGenerator
+	{
+		private static readonly Random Random = new Random();
+
+		public static Sudoku AddNumbers(int size, SudokuDifficulty difficulty)
+		{
+			if (difficulty == SudokuDifficulty.None)
+			{
+				throw new ArgumentException(nameof (difficulty));
+			}
+
+			Sudoku sudoku = new Sudoku(size, difficulty);
+			SudokuGenerator.FillNumbers(sudoku, 0, 0);
+			return sudoku;
+		}
+
+		private static bool FillNumbers(Sudoku sudoku, int row, int column)
+		{
+			List<int> possible = new List<int>(sudoku.GetPossible(row, column));
+			SudokuGenerator.ShuffleNumbers(possible);
+
+			while (possible.Count != 0)
+			{
+				sudoku[row, column] = possible[0];
+				int nextColumn = column + 1, nextRow = row;
+
+				if (nextColumn == sudoku.Size)
+				{
+					nextRow ++;
+					nextColumn = 0;
+				}
+
+				if (nextRow == sudoku.Size || SudokuGenerator.FillNumbers(sudoku, nextRow, nextColumn))
+				{
+					return true;
+				}
+
+				possible.RemoveAt(0);
+			}
+
+			sudoku[row, column] = 0;
+			return false;
+		}
+
+		public static Sudoku Generate(int size, SudokuDifficulty difficulty)
+		{
+			Sudoku sudoku = SudokuGenerator.AddNumbers(size, difficulty);
+			SudokuGenerator.RemoveNumbers(sudoku);
+			return sudoku;
+		}
+
+		public static void RemoveNumbers(Sudoku sudoku)
+		{
+			if (sudoku is null)
+			{
+				throw new NotImplementedException(nameof (sudoku));
+			}
+
+			throw new NotImplementedException();
+		}
+
+		private static void ShuffleNumbers<T>(List<T> array)
+		{
+			for (int i = 0; i < array.Count - 1; i ++)
+			{
+				int j = SudokuGenerator.Random.Next(i, array.Count);
+				T temp = array[j];
+				array[j] = array[i];
+				array[i] = temp;
+			}
+		}
+	}
+}
