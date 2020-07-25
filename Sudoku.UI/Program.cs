c063648b03f1e186d2ac6365b7eb118659751c08
Christopher Shipstone <christopher.shipstone98@gmail.com>
2020-07-25 22:42:53 +0100

    Added argument parsing and test

---
+++ Sudoku.UI/Program.cs
@@ -0,0 +1,218 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using System.Text.RegularExpressions;
+
+[assembly: InternalsVisibleTo("Sudoku.UI.Test")]
+namespace Sudoku.UI
+{
+	public static class Program
+	{
+		internal const int UsageMessageWarning = 1;
+
+		private const int GenerateCommand = 1;
+		private const int PrintCommand = 2;
+		private const int SolveCommand = 3;
+
+		private const String UsageMessage = "Usage: sudoku command [options]\n\nOptions\n\t- GENERATE: Generate a new sudoku puzzle, must specify size with -s and difficulty with -d. Size must be positive square integer and difficulty must be easy, medium or hard. Can optionally specify an output filename with -o\n\t- PRINT: Print an existing sudoku puzzle to the screen, must specify filename with -f\n\t-SOLVE: Solves an existing sudoku puzzle, must specify filename with -f. Can optionally specify an output filename with -o\n\nAll options are NOT case sensitive; however, some operating systems may treat entered filenames as case sensitive (Windows doesn't but Mac OS and Linux generally do)";
+
+		private static void Generate(int size, SudokuDifficulty difficulty, String outfile)
+		{
+			//throw new NotImplementedException();
+		}
+
+		public static int Main(String[] args)
+		{
+			if (!Program.ParseArguments(args, out int command, out int size, out SudokuDifficulty difficulty, out String filename, out String outfile))
+			{
+				Console.WriteLine(Program.UsageMessage);
+				return Program.UsageMessageWarning;
+			}
+
+			switch (command)
+			{
+				case Program.GenerateCommand:
+					Program.Generate(size, difficulty, outfile);
+					break;
+				case Program.PrintCommand:
+					Program.Print(filename);
+					break;
+				case Program.SolveCommand:
+					Program.Solve(filename, outfile);
+					break;
+				default:
+					Console.WriteLine(Program.UsageMessage);
+					return Program.UsageMessageWarning;
+			}
+
+			return 0;
+		}
+
+		private static void Print(String filename)
+		{
+			//throw new NotImplementedException();
+		}
+
+		private static bool ParseArguments(String[] args, out int command, out int size, out SudokuDifficulty difficulty, out String filename, out String outfile)
+		{
+			command = size = 0;
+			filename = outfile = null;
+			difficulty = SudokuDifficulty.None;
+
+			try
+			{
+				switch (args[0].ToLower())
+				{
+					case "g":
+					case "gen":
+					case "generate":
+						command = Program.GenerateCommand;
+						break;
+
+					case "p":
+					case "print":
+						command = Program.PrintCommand;
+						break;
+
+					case "s":
+					case "solve":
+						command = Program.SolveCommand;
+						break;
+
+					default:
+						return false;
+				}
+
+				for (int i = 1; i < args.Length; i ++)
+				{
+					if (String.IsNullOrWhiteSpace(args[i]))
+					{
+						continue;
+					}
+
+					String arg = Regex.Replace(args[i].ToLower(), @"\s+", "");
+
+					if (arg[0] == '-')
+					{
+						if (arg.Length > 2)
+						{
+							return false;
+						}
+
+						String param = Regex.Replace(args[i + 1].ToLower(), @"\s+", "");
+
+						switch (arg[1])
+						{
+							case 'd':
+								if (command != Program.GenerateCommand || difficulty != SudokuDifficulty.None)
+								{
+									return false;
+								}
+
+								try
+								{
+									difficulty = (SudokuDifficulty) Enum.Parse(typeof (SudokuDifficulty), param, true);
+								}
+
+								catch
+								{
+									difficulty = (SudokuDifficulty) Int32.Parse(param);
+								}
+
+								if (difficulty == SudokuDifficulty.None)
+								{
+									return false;
+								}
+
+								break;
+
+							case 'f':
+								if (command == Program.GenerateCommand || !(filename is null) || String.IsNullOrWhiteSpace(param))
+								{
+									return false;
+								}
+
+								filename = param;
+								break;
+
+							case 'o':
+								if (command == Program.PrintCommand || !(outfile is null) || String.IsNullOrWhiteSpace(param))
+								{
+									return false;
+								}
+
+								outfile = param;
+								break;
+
+							case 's':
+								if (command != Program.GenerateCommand || size != 0)
+								{
+									return false;
+								}
+
+								size = Int32.Parse(param);
+
+								if (!Sudoku.VerifySize(size))
+								{
+									return false;
+								}
+
+								break;
+
+							default:
+								return false;
+						}
+
+						i ++;
+					}
+
+					else
+					{
+						return false;
+					}
+				}
+			}
+
+			catch
+			{
+				return false;
+			}
+
+			switch (command)
+			{
+				case Program.GenerateCommand:
+					if (!Sudoku.VerifySize(size) || difficulty == SudokuDifficulty.None)
+					{
+						return false;
+					}
+
+					break;
+
+				case Program.PrintCommand:
+					if (String.IsNullOrWhiteSpace(filename))
+					{
+						return false;
+					}
+
+					break;
+
+				case Program.SolveCommand:
+					if (String.IsNullOrWhiteSpace(filename) || !(outfile is null) && String.IsNullOrWhiteSpace(outfile))
+					{
+						return false;
+					}
+
+					break;
+
+				default:
+					return false;
+			}
+
+			return true;
+		}
+
+		private static void Solve(String filename, String outfile)
+		{
+			//throw new NotImplementedException();
+		}
+	}
+}
