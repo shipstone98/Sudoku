cdd240eea8e8c702baef91a7385d9e1e0d61c515
Christopher Shipstone <christopher.shipstone98@gmail.com>
2020-07-25 20:48:59 +0100

    Added more possible tests

--- Sudoku.Test/SudokuTest.cs
+++ Sudoku.Test/SudokuTest.cs
@@ -52,15 +52,74 @@ namespace Sudoku.Test
 
 		private int Generate(bool includeUpperLimit = false) => includeUpperLimit ? this.Random.Next(SudokuTest.Size + 1) : this.Random.Next(SudokuTest.Size);
 
+		private void TestSetNumberFromZeroToNumber(out int row, out int column, out int number)
+		{
+			row = this.Generate();
+			column = this.Generate();
+			number = this.Generate(true);
+			List<int> affectedPossibleList = new List<int>(this.Possible);
+			affectedPossibleList.Remove(number);
+			int[] affectedPossible = affectedPossibleList.ToArray();
+			this.Sudoku.GetStartRowColumn(row, column, out int startRow, out int startColumn);
+			this.Sudoku[row, column] = number;
+
+			for (int i = 0; i < SudokuTest.Size; i ++)
+			{
+				for (int j = 0; j < SudokuTest.Size; j ++)
+				{
+					this.Sudoku.GetStartRowColumn(i, j, out int currentStartRow, out int currentStartColumn);
+
+					if (i == row || j == column || currentStartRow == startRow && currentStartColumn == startColumn)
+					{
+						//Check against affectedPossible
+						Assert.IsTrue(SudokuTest.ArraysEqual<int>(affectedPossible, this.Sudoku.GetPossible(i, j)));
+					}
+
+					else
+					{
+						Assert.IsTrue(SudokuTest.ArraysEqual<int>(this.Possible, this.Sudoku.GetPossible(i, j)));
+					}
+				}
+			}
+		}
+
 		[TestMethod]
 		public void TestSetNumberFromZeroToNumber()
 		{
-			int newNumber = this.Generate(true), row = this.Generate(), column = this.Generate();
+			this.TestSetNumberFromZeroToNumber(out int row, out int column, out int number);
+		}
+
+		[TestMethod]
+		public void TestSetNumberFromNumberToZero()
+		{
+			this.TestSetNumberFromZeroToNumber(out int row, out int column, out int number);
+			this.Sudoku[row, column] = 0;
+
+			for (int i = 0; i < SudokuTest.Size; i++)
+			{
+				for (int j = 0; j < SudokuTest.Size; j++)
+				{
+					Assert.IsTrue(SudokuTest.ArraysEqual<int>(this.Possible, this.Sudoku.GetPossible(i, j)));
+				}
+			}
+		}
+
+		[TestMethod]
+		public void TestSetNumberFromNumberToNumber()
+		{
+			this.TestSetNumberFromZeroToNumber(out int row, out int column, out int oldNumber);
+			int number;
+
+			do
+			{
+				number = this.Generate(true);
+			} while (number != oldNumber);
+
 			List<int> affectedPossibleList = new List<int>(this.Possible);
-			affectedPossibleList.Remove(newNumber);
+			affectedPossibleList.Remove(number);
 			int[] affectedPossible = affectedPossibleList.ToArray();
 			this.Sudoku.GetStartRowColumn(row, column, out int startRow, out int startColumn);
-			this.Sudoku[row, column] = newNumber;
+			this.Sudoku[row, column] = number;
 
 			for (int i = 0; i < SudokuTest.Size; i ++)
 			{
