1bf374af08260ed1f17533d46e7ef624f1c6ce6d
Christopher Shipstone <christopher.shipstone98@gmail.com>
2020-07-25 18:47:07 +0100

    Added class properties to SudokuCell

--- Sudoku/SudokuCell.cs
+++ Sudoku/SudokuCell.cs
@@ -1,10 +1,62 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace Sudoku
 {
 	internal class SudokuCell
 	{
+		private int _Number;
+		private int _Solution;
+
+		internal bool Correct => this._Number == this._Solution && this._Number != 0;
+
+		internal int Number
+		{
+			get => this._Number;
+
+			set
+			{
+				if (this.ReadOnly)
+				{
+					throw new SudokuCellReadOnlyException();
+				}
+
+				if (value < 0)
+				{
+					throw new ArgumentOutOfRangeException(nameof (value));
+				}
+
+				this._Number = value;
+			}
+		}
+
+		internal bool ReadOnly { get; private set; }
+
+		internal int Solution
+		{
+			get => this._Solution;
+
+			set
+			{
+				if (this.ReadOnly)
+				{
+					throw new SudokuCellReadOnlyException();
+				}
+
+				if (value <= 0)
+				{
+					throw new ArgumentOutOfRangeException(nameof(value));
+				}
+
+				this._Solution = value;
+			}
+		}
+
+		internal SudokuCell()
+		{
+			this._Number = this._Solution = 0;
+			this.ReadOnly = false;
+		}
+
+		internal void MakeReadOnly() => this.ReadOnly = true;
 	}
 }
