727857c63a1f6fd63b91c4ce2ebc568529dc02cf
Christopher Shipstone <christopher.shipstone98@gmail.com>
2020-07-26 11:44:38 +0100

    Added new type of move that doesn't fill a cell, only removes possible numbers

---
+++ Sudoku/SudokuPossibleMove.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Sudoku
+{
+	public class SudokuPossibleMove : SudokuMove
+	{
+		private readonly int[] _ColumnsAffected;
+		private readonly int[] _NumbersAffected;
+		private readonly int[] _RowsAfffected;
+
+		public int[] ColumnsAffected => this._ColumnsAffected;
+		public int[] NumbersAffected => this._NumbersAffected;
+		public int[] RowsAffected => this._RowsAfffected;
+
+		public SudokuPossibleMove(int row, int column, int number, SudokuPattern pattern, int[] possible, int[] rowsAffected, int[] columnsAffected, int[] numbersAffected) : base(row, column, number, pattern, possible)
+		{
+			this._ColumnsAffected = columnsAffected ?? new int[0];
+			this._NumbersAffected = numbersAffected ?? new int[0];
+			this._RowsAfffected = rowsAffected ?? new int[0];
+		}
+
+		public override String ToString() => this.ToString(SudokuMove.DefaultPatternNamePadding);
+
+		public virtual String ToString(int patternNamePadding)
+		{
+			int[] rows = new int[this._RowsAfffected.Length];
+			int[] columns = new int[this._ColumnsAffected.Length];
+
+			for (int i = 0; i < rows.Length; i ++)
+			{
+				rows[i] ++;
+			}
+
+			for (int i = 0; i < columns.Length; i ++)
+			{
+				columns[i] ++;
+			}
+
+			return String.Join("\t", this.ToString(patternNamePadding, true), String.Join(",", rows).PadRight(patternNamePadding), String.Join(",", columns).PadRight(patternNamePadding), String.Join(",", this._NumbersAffected).PadRight(patternNamePadding));
+		}
+	}
+}
