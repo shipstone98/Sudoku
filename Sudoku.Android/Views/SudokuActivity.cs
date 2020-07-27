88781bbfb917d04efb52af553ef4bbd4979e116e
Christopher Shipstone <christopher.shipstone98@gmail.com>
2020-07-27 23:33:01 +0100

    Can now navigate to SudokuActivity

---
+++ Sudoku.Android/Views/SudokuActivity.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+using Android.App;
+using Android.Content.PM;
+using Android.OS;
+using Android.Runtime;
+
+using AndroidX.AppCompat.App;
+
+using Xamarin.Essentials;
+
+namespace Sudoku.Android.Views
+{
+	[Activity(Label = "@string/app_name", Theme = "@style/AppTheme")]
+	public class SudokuActivity : AppCompatActivity
+	{
+		protected override void OnCreate(Bundle savedInstanceState)
+		{
+			base.OnCreate(savedInstanceState);
+			Platform.Init(this, savedInstanceState);
+			// Set our view from the "main" layout resource
+			this.SetContentView(Resource.Layout.activity_sudoku);
+		}
+
+		public override void OnRequestPermissionsResult(int requestCode, String[] permissions, [GeneratedEnum] Permission[] grantResults)
+		{
+			Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+			base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+		}
+	}
+}
