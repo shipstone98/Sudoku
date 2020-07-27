7d4a81e531e55f4b305763a4375c91ca8a67b315
Christopher Shipstone <christopher.shipstone98@gmail.com>
2020-07-27 22:38:42 +0100

    Added Android project scaffolding

---
+++ Sudoku.Android/Views/MainActivity.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+using Android.App;
+using Android.Content.PM;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+
+using Xamarin.Essentials;
+
+namespace Sudoku.Android.Views
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Platform.Init(this, savedInstanceState);
+            // Set our view from the "main" layout resource
+            this.SetContentView(Resource.Layout.activity_main);
+        }
+
+        public override void OnRequestPermissionsResult(int requestCode, String[] permissions, [GeneratedEnum] Permission[] grantResults)
+        {
+            Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
