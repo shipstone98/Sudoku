c733891f53a690a318abe98bd817fda3fb934de2
Christopher Shipstone <christopher.shipstone98@gmail.com>
2020-07-27 23:33:01 +0100

    Can now navigate to SudokuActivity

--- Sudoku.Android/Resources/Resource.designer.cs
+++ Sudoku.Android/Resources/Resource.designer.cs
@@ -3912,110 +3912,113 @@ namespace Sudoku.Android
 			// aapt resource value: 0x7F0800AF
 			public const int submit_area = 2131230895;
 			
+			// aapt resource value: 0x7F0800B0
+			public const int sudoku_fragment = 2131230896;
+			
 			// aapt resource value: 0x7F080005
 			public const int SYM = 2131230725;
 			
-			// aapt resource value: 0x7F0800B0
-			public const int tabMode = 2131230896;
-			
 			// aapt resource value: 0x7F0800B1
-			public const int tag_transition_group = 2131230897;
+			public const int tabMode = 2131230897;
 			
 			// aapt resource value: 0x7F0800B2
-			public const int tag_unhandled_key_event_manager = 2131230898;
+			public const int tag_transition_group = 2131230898;
 			
 			// aapt resource value: 0x7F0800B3
-			public const int tag_unhandled_key_listeners = 2131230899;
+			public const int tag_unhandled_key_event_manager = 2131230899;
 			
 			// aapt resource value: 0x7F0800B4
-			public const int text = 2131230900;
+			public const int tag_unhandled_key_listeners = 2131230900;
 			
 			// aapt resource value: 0x7F0800B5
-			public const int text2 = 2131230901;
-			
-			// aapt resource value: 0x7F0800BA
-			public const int textinput_counter = 2131230906;
-			
-			// aapt resource value: 0x7F0800BB
-			public const int textinput_error = 2131230907;
-			
-			// aapt resource value: 0x7F0800BC
-			public const int textinput_helper_text = 2131230908;
+			public const int text = 2131230901;
 			
 			// aapt resource value: 0x7F0800B6
-			public const int textSpacerNoButtons = 2131230902;
+			public const int text2 = 2131230902;
 			
-			// aapt resource value: 0x7F0800B7
-			public const int textSpacerNoTitle = 2131230903;
+			// aapt resource value: 0x7F0800BB
+			public const int textinput_counter = 2131230907;
 			
-			// aapt resource value: 0x7F0800B8
-			public const int textStart = 2131230904;
-			
-			// aapt resource value: 0x7F0800B9
-			public const int text_input_password_toggle = 2131230905;
+			// aapt resource value: 0x7F0800BC
+			public const int textinput_error = 2131230908;
 			
 			// aapt resource value: 0x7F0800BD
-			public const int time = 2131230909;
+			public const int textinput_helper_text = 2131230909;
+			
+			// aapt resource value: 0x7F0800B7
+			public const int textSpacerNoButtons = 2131230903;
+			
+			// aapt resource value: 0x7F0800B8
+			public const int textSpacerNoTitle = 2131230904;
+			
+			// aapt resource value: 0x7F0800B9
+			public const int textStart = 2131230905;
+			
+			// aapt resource value: 0x7F0800BA
+			public const int text_input_password_toggle = 2131230906;
 			
 			// aapt resource value: 0x7F0800BE
-			public const int title = 2131230910;
+			public const int time = 2131230910;
 			
 			// aapt resource value: 0x7F0800BF
-			public const int titleDividerNoCustom = 2131230911;
+			public const int title = 2131230911;
 			
 			// aapt resource value: 0x7F0800C0
-			public const int title_template = 2131230912;
+			public const int titleDividerNoCustom = 2131230912;
 			
 			// aapt resource value: 0x7F0800C1
-			public const int top = 2131230913;
+			public const int title_template = 2131230913;
 			
 			// aapt resource value: 0x7F0800C2
-			public const int topPanel = 2131230914;
+			public const int top = 2131230914;
 			
 			// aapt resource value: 0x7F0800C3
-			public const int touch_outside = 2131230915;
+			public const int topPanel = 2131230915;
 			
 			// aapt resource value: 0x7F0800C4
-			public const int transition_current_scene = 2131230916;
+			public const int touch_outside = 2131230916;
 			
 			// aapt resource value: 0x7F0800C5
-			public const int transition_layout_save = 2131230917;
+			public const int transition_current_scene = 2131230917;
 			
 			// aapt resource value: 0x7F0800C6
-			public const int transition_position = 2131230918;
+			public const int transition_layout_save = 2131230918;
 			
 			// aapt resource value: 0x7F0800C7
-			public const int transition_scene_layoutid_cache = 2131230919;
+			public const int transition_position = 2131230919;
 			
 			// aapt resource value: 0x7F0800C8
-			public const int transition_transform = 2131230920;
+			public const int transition_scene_layoutid_cache = 2131230920;
 			
 			// aapt resource value: 0x7F0800C9
-			public const int uniform = 2131230921;
+			public const int transition_transform = 2131230921;
 			
 			// aapt resource value: 0x7F0800CA
-			public const int unlabeled = 2131230922;
+			public const int uniform = 2131230922;
 			
 			// aapt resource value: 0x7F0800CB
-			public const int up = 2131230923;
+			public const int unlabeled = 2131230923;
 			
 			// aapt resource value: 0x7F0800CC
-			public const int useLogo = 2131230924;
+			public const int up = 2131230924;
 			
 			// aapt resource value: 0x7F0800CD
-			public const int view_offset_helper = 2131230925;
+			public const int useLogo = 2131230925;
 			
 			// aapt resource value: 0x7F0800CE
-			public const int visible = 2131230926;
+			public const int view_offset_helper = 2131230926;
 			
 			// aapt resource value: 0x7F0800CF
-			public const int withText = 2131230927;
+			public const int visible = 2131230927;
 			
 			// aapt resource value: 0x7F0800D0
-			public const int wrap = 2131230928;
+			public const int withText = 2131230928;
 			
 			// aapt resource value: 0x7F0800D1
-			public const int wrap_content = 2131230929;
+			public const int wrap = 2131230929;
+			
+			// aapt resource value: 0x7F0800D2
+			public const int wrap_content = 2131230930;
 			
 			static Id()
 			{
@@ -4201,91 +4204,97 @@ namespace Sudoku.Android
 			public const int activity_main = 2131427356;
 			
 			// aapt resource value: 0x7F0B001D
-			public const int browser_actions_context_menu_page = 2131427357;
+			public const int activity_sudoku = 2131427357;
 			
 			// aapt resource value: 0x7F0B001E
-			public const int browser_actions_context_menu_row = 2131427358;
+			public const int browser_actions_context_menu_page = 2131427358;
 			
 			// aapt resource value: 0x7F0B001F
-			public const int design_bottom_navigation_item = 2131427359;
+			public const int browser_actions_context_menu_row = 2131427359;
 			
 			// aapt resource value: 0x7F0B0020
-			public const int design_bottom_sheet_dialog = 2131427360;
+			public const int design_bottom_navigation_item = 2131427360;
 			
 			// aapt resource value: 0x7F0B0021
-			public const int design_layout_snackbar = 2131427361;
+			public const int design_bottom_sheet_dialog = 2131427361;
 			
 			// aapt resource value: 0x7F0B0022
-			public const int design_layout_snackbar_include = 2131427362;
+			public const int design_layout_snackbar = 2131427362;
 			
 			// aapt resource value: 0x7F0B0023
-			public const int design_layout_tab_icon = 2131427363;
+			public const int design_layout_snackbar_include = 2131427363;
 			
 			// aapt resource value: 0x7F0B0024
-			public const int design_layout_tab_text = 2131427364;
+			public const int design_layout_tab_icon = 2131427364;
 			
 			// aapt resource value: 0x7F0B0025
-			public const int design_menu_item_action_area = 2131427365;
+			public const int design_layout_tab_text = 2131427365;
 			
 			// aapt resource value: 0x7F0B0026
-			public const int design_navigation_item = 2131427366;
+			public const int design_menu_item_action_area = 2131427366;
 			
 			// aapt resource value: 0x7F0B0027
-			public const int design_navigation_item_header = 2131427367;
+			public const int design_navigation_item = 2131427367;
 			
 			// aapt resource value: 0x7F0B0028
-			public const int design_navigation_item_separator = 2131427368;
+			public const int design_navigation_item_header = 2131427368;
 			
 			// aapt resource value: 0x7F0B0029
-			public const int design_navigation_item_subheader = 2131427369;
+			public const int design_navigation_item_separator = 2131427369;
 			
 			// aapt resource value: 0x7F0B002A
-			public const int design_navigation_menu = 2131427370;
+			public const int design_navigation_item_subheader = 2131427370;
 			
 			// aapt resource value: 0x7F0B002B
-			public const int design_navigation_menu_item = 2131427371;
+			public const int design_navigation_menu = 2131427371;
 			
 			// aapt resource value: 0x7F0B002C
-			public const int design_text_input_password_icon = 2131427372;
+			public const int design_navigation_menu_item = 2131427372;
 			
 			// aapt resource value: 0x7F0B002D
-			public const int fragment_menu = 2131427373;
+			public const int design_text_input_password_icon = 2131427373;
 			
 			// aapt resource value: 0x7F0B002E
-			public const int mtrl_layout_snackbar = 2131427374;
+			public const int fragment_menu = 2131427374;
 			
 			// aapt resource value: 0x7F0B002F
-			public const int mtrl_layout_snackbar_include = 2131427375;
+			public const int fragment_sudoku = 2131427375;
 			
 			// aapt resource value: 0x7F0B0030
-			public const int notification_action = 2131427376;
+			public const int mtrl_layout_snackbar = 2131427376;
 			
 			// aapt resource value: 0x7F0B0031
-			public const int notification_action_tombstone = 2131427377;
+			public const int mtrl_layout_snackbar_include = 2131427377;
 			
 			// aapt resource value: 0x7F0B0032
-			public const int notification_template_custom_big = 2131427378;
+			public const int notification_action = 2131427378;
 			
 			// aapt resource value: 0x7F0B0033
-			public const int notification_template_icon_group = 2131427379;
+			public const int notification_action_tombstone = 2131427379;
 			
 			// aapt resource value: 0x7F0B0034
-			public const int notification_template_part_chronometer = 2131427380;
+			public const int notification_template_custom_big = 2131427380;
 			
 			// aapt resource value: 0x7F0B0035
-			public const int notification_template_part_time = 2131427381;
+			public const int notification_template_icon_group = 2131427381;
 			
 			// aapt resource value: 0x7F0B0036
-			public const int select_dialog_item_material = 2131427382;
+			public const int notification_template_part_chronometer = 2131427382;
 			
 			// aapt resource value: 0x7F0B0037
-			public const int select_dialog_multichoice_material = 2131427383;
+			public const int notification_template_part_time = 2131427383;
 			
 			// aapt resource value: 0x7F0B0038
-			public const int select_dialog_singlechoice_material = 2131427384;
+			public const int select_dialog_item_material = 2131427384;
 			
 			// aapt resource value: 0x7F0B0039
-			public const int support_simple_spinner_dropdown_item = 2131427385;
+			public const int select_dialog_multichoice_material = 2131427385;
+			
+			// aapt resource value: 0x7F0B003A
+			public const int select_dialog_singlechoice_material = 2131427386;
+			
+			// aapt resource value: 0x7F0B003B
+			public const int support_simple_spinner_dropdown_item = 2131427387;
 			
 			static Layout()
 			{
--- Sudoku.Android/Views/MenuFragment.cs
+++ Sudoku.Android/Views/MenuFragment.cs
@@ -1,12 +1,51 @@
-﻿using Android.OS;
+﻿using System;
+
+using Android.Content;
+using Android.OS;
 using Android.Views;
+using Android.Widget;
 
 using AndroidX.Fragment.App;
 
 namespace Sudoku.Android.Views
 {
+	public class ButtonOnClickListener : Java.Lang.Object, View.IOnClickListener
+	{
+		public const String GenerateAction = "Generate";
+
+		public String Action { get; }
+		public FragmentActivity Activity { get; }
+
+		public ButtonOnClickListener(String action, FragmentActivity activity)
+		{
+			this.Action = action;
+			this.Activity = activity;
+		}
+
+		public void OnClick(View view)
+		{
+			switch (this.Action)
+			{
+				case ButtonOnClickListener.GenerateAction:
+					Intent intent = new Intent(this.Activity.ApplicationContext, Java.Lang.Class.FromType(typeof (SudokuActivity)));
+					this.Activity.StartActivity(intent);
+					//Open new activity
+					break;
+				default:
+					throw new NotImplementedException();
+			}
+		}
+	}
+
 	public class MenuFragment : Fragment
 	{
+		public override void OnActivityCreated(Bundle savedInstanceState)
+		{
+			base.OnActivityCreated(savedInstanceState);
+			Button generateButton = this.View.FindViewById<Button>(Resource.Id.new_button);
+			generateButton.SetOnClickListener(new ButtonOnClickListener(ButtonOnClickListener.GenerateAction, this.Activity));
+		}
+
 		public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) => inflater.Inflate(Resource.Layout.fragment_menu, container, false);
 	}
 }
