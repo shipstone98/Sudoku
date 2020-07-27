cc1a59ad49ed44557abf1c99e3ed55fcc452a2fa
Christopher Shipstone <christopher.shipstone98@gmail.com>
2020-07-27 15:57:36 +0100

    Added documentation

--- Sudoku/SudokuPattern.cs
+++ Sudoku/SudokuPattern.cs
@@ -1,11 +1,33 @@
 namespace Sudoku
 {
+	/// <summary>
+	/// Specified constants that define moves used to solve <see cref="Sudoku"/> puzzles.
+	/// </summary>
     public enum SudokuPattern
     {
+		/// <summary>
+		/// No specified pattern. This can be used when solving using recursion, rather than pattern matching.
+		/// </summary>
         None,
+
+		/// <summary>
+		/// Defines a claiming locked candidate.
+		/// </summary>
         ClaimingCandidate,
+
+		/// <summary>
+		/// Defines a hidden single.
+		/// </summary>
         HiddenSingle,
+
+		/// <summary>
+		/// Defines a naked single.
+		/// </summary>
         NakedSingle,
+
+		/// <summary>
+		/// Defines a pointing locked candidate.
+		/// </summary>
         PointingCandidate
     }
 }
