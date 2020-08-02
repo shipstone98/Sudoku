f049fb8840a29df106c74dcb49bb6729eefc894e
Christopher Shipstone <christopher.shipstone98@gmail.com>
2020-08-03 00:59:59 +0100

    Added constructor and property tests

---
+++ Sudoku.Test/SudokuMoveTest.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Sudoku.Test
+{
+	[TestClass]
+	public class SudokuMoveTest
+	{
+		private SudokuMove Move;
+
+		[TestInitialize]
+		public void Initialize()
+		{
+			const int NUMBER = SudokuPuzzle.MaximumSupportedSize;
+			this.Move = new SudokuMove(NUMBER, NUMBER, NUMBER, SudokuPattern.HiddenSingle, new int[] { NUMBER });
+		}
+
+		[TestMethod]
+		public void TestConstructor()
+		{
+			const SudokuPattern BAD_PATTERN = SudokuPattern.None;
+			const SudokuPattern GOOD_PATTERN = SudokuPattern.ClaimingCandidate;
+			const int BAD_NUMBER = -1;
+			const int GOOD_NUMBER = SudokuPuzzle.MaximumSupportedSize;
+			int[] badArray = new int[] { -1 };
+			int[] emptyArray = new int[0];
+			int[] goodArray = new int[] { 1, GOOD_NUMBER };
+			Assert.ThrowsException<ArgumentNullException>(() => new SudokuMove(null, null, null, GOOD_PATTERN, null));
+			Assert.ThrowsException<ArgumentNullException>(() => new SudokuMove(goodArray, null, null, GOOD_PATTERN, null));
+			Assert.ThrowsException<ArgumentNullException>(() => new SudokuMove(goodArray, goodArray, null, GOOD_PATTERN, null));
+			Assert.ThrowsException<ArgumentNullException>(() => new SudokuMove(goodArray, goodArray, goodArray, GOOD_PATTERN, null));
+			Assert.ThrowsException<ArgumentException>(() => new SudokuMove(badArray, badArray, badArray, BAD_PATTERN, badArray));
+			Assert.ThrowsException<ArgumentException>(() => new SudokuMove(goodArray, badArray, badArray, BAD_PATTERN, badArray));
+			Assert.ThrowsException<ArgumentException>(() => new SudokuMove(goodArray, goodArray, badArray, BAD_PATTERN, badArray));
+			Assert.ThrowsException<ArgumentException>(() => new SudokuMove(goodArray, goodArray, goodArray, BAD_PATTERN, badArray));
+			Assert.ThrowsException<ArgumentException>(() => new SudokuMove(emptyArray, emptyArray, emptyArray, BAD_PATTERN, emptyArray));
+			Assert.ThrowsException<ArgumentException>(() => new SudokuMove(goodArray, emptyArray, emptyArray, BAD_PATTERN, emptyArray));
+			Assert.ThrowsException<ArgumentException>(() => new SudokuMove(goodArray, goodArray, emptyArray, BAD_PATTERN, emptyArray));
+			Assert.ThrowsException<ArgumentException>(() => new SudokuMove(goodArray, goodArray, goodArray, BAD_PATTERN, emptyArray));
+			Assert.ThrowsException<ArgumentException>(() => new SudokuMove(goodArray, goodArray, new int[] { GOOD_NUMBER, 1, 1 }, GOOD_PATTERN, goodArray));
+			Assert.ThrowsException<ArgumentOutOfRangeException>(() => new SudokuMove(BAD_NUMBER, BAD_NUMBER, BAD_NUMBER, BAD_PATTERN, goodArray));
+			Assert.ThrowsException<ArgumentOutOfRangeException>(() => new SudokuMove(GOOD_NUMBER, BAD_NUMBER, BAD_NUMBER, BAD_PATTERN, goodArray));
+			Assert.ThrowsException<ArgumentOutOfRangeException>(() => new SudokuMove(GOOD_NUMBER, GOOD_NUMBER, BAD_NUMBER, BAD_PATTERN, goodArray));
+			Assert.ThrowsException<ArgumentException>(() => new SudokuMove(GOOD_NUMBER, GOOD_NUMBER, GOOD_NUMBER, BAD_PATTERN, goodArray));
+
+			try
+			{
+				new SudokuMove(goodArray, goodArray, goodArray, GOOD_PATTERN, goodArray);
+			}
+
+			catch (Exception ex)
+			{
+				Assert.Fail($"Throw exception: {ex}");
+			}
+		}
+
+		[TestMethod]
+		public void TestProperties()
+		{
+			int[] array = new int[] { 1, SudokuPuzzle.MaximumSupportedSize };
+			SudokuMove move = new SudokuMove(array, array, array, SudokuPattern.HiddenSingle, array);
+			Assert.IsTrue(SudokuCellTest.Equal(array, move.Columns));
+			Assert.IsTrue(SudokuCellTest.Equal(array, move.Numbers));
+			Assert.IsTrue(SudokuCellTest.Equal(array, move.Possible));
+			Assert.IsTrue(SudokuCellTest.Equal(array, move.Rows));
+		}
+	}
+}
