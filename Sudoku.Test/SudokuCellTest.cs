414baaf46f547f7c1c130651f47b69bd31af791e
Christopher Shipstone <christopher.shipstone98@gmail.com>
2020-08-03 00:03:16 +0100

    Added IsCorrect test

---
+++ Sudoku.Test/SudokuCellTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Sudoku.Test
+{
+	[TestClass]
+	public class SudokuCellTest
+	{
+		private SudokuCell Cell;
+
+		[TestInitialize]
+		public void Initialize()
+		{
+			this.Cell = new SudokuCell();
+		}
+
+		[TestMethod]
+		public void TestIsCorrect()
+		{
+			Assert.IsFalse(this.Cell.IsCorrect);
+			this.Cell.Solution = SudokuPuzzle.MaximumSupportedSize;
+			Assert.IsFalse(this.Cell.IsCorrect);
+			this.Cell.Solution = SudokuPuzzle.MaximumSupportedSize - 1;
+			Assert.IsFalse(this.Cell.IsCorrect);
+			this.Cell.Number = this.Cell.Solution;
+			Assert.IsTrue(this.Cell.IsCorrect);
+		}
+	}
+}
