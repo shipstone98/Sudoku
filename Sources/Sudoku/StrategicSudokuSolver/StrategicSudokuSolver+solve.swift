169452b8ad99bb26cd99a5e60271c398dbfd9061
Christopher Shipstone <christopher.shipstone98@gmail.com>
2026-03-31 09:49:08 +0100

    Refactored code

---
+++ Sources/Sudoku/StrategicSudokuSolver/StrategicSudokuSolver+solve.swift
@@ -0,0 +1,62 @@
+//
+//  StrategicSudokuSolver+solve.swift
+//  Sudoku
+//
+//  Created by Christopher Shipstone on 30/03/2026.
+//
+
+public extension StrategicSudokuSolver {
+    @discardableResult
+    mutating func solve<T>(
+        for strategy: SudokuSolverStrategy,
+        using generator: inout T
+    ) -> SudokuSolverMove? where T : RandomNumberGenerator {
+        let move: SudokuSolverMove?
+        
+        switch strategy {
+        case .fullHouse:
+            move = self.solveFullHouse(using: &generator)
+        case .nakedSingle:
+            move = self.solveNakedSingle(using: &generator)
+        case .hiddenSingle:
+            move = self.solveHiddenSingle(using: &generator)
+        case .pointingCandidate:
+            move = self.solvePointingCandidate(using: &generator)
+        case .claimingCandidate:
+            move = self.solveClaimingCandidate(using: &generator)
+        case .nakedPair:
+            move = self.solveNakedPair(using: &generator)
+        }
+        
+        guard let move else {
+            return nil
+        }
+        
+        for location in move.locations {
+            let index = location.row * 9 + location.column
+            
+            if let addedValue = location.addedValue {
+                for index in ArraySudoku.peers(location.row, location.column) {
+                    if var candidates = self.candidates[index] {
+                        candidates.remove(addedValue)
+                        self.candidates[index] = candidates
+                    }
+                }
+                
+                self.candidates.removeValue(forKey: index)
+                self.sudoku.array[index] = addedValue
+            } else {
+                if var candidates = self.candidates[index] {
+                    for candidate in location.removedCandidates {
+                        candidates.remove(candidate)
+                    }
+                    
+                    self.candidates[index] = candidates
+                }
+            }
+        }
+        
+        self.moves.append(move)
+        return move
+    }
+}
--- Sources/Sudoku/StrategicSudokuSolver/StrategicSudokuSolver.swift
+++ Sources/Sudoku/StrategicSudokuSolver/StrategicSudokuSolver.swift
@@ -7,9 +7,9 @@
 
 @frozen
 public struct StrategicSudokuSolver : Codable, Hashable, Sendable, SudokuSolver {
-    internal private(set) var candidates: [Int: Set<Int>]
-    public private(set) var moves: [SudokuSolverMove]
-    public private(set) var sudoku: ArraySudoku
+    internal var candidates: [Int: Set<Int>]
+    public internal(set) var moves: [SudokuSolverMove]
+    public internal(set) var sudoku: ArraySudoku
     
     public init<S>(_ sudoku: S) where S : SudokuProtocol {
         let sudoku = ArraySudoku(sudoku)
@@ -33,69 +33,7 @@ public struct StrategicSudokuSolver : Codable, Hashable, Sendable, SudokuSolver 
     }
     
     @discardableResult
-    public mutating func solve<T>(using generator: inout T) -> Bool where T : RandomNumberGenerator {
-        var isSolved: Bool
-        
-        repeat {
-            isSolved = SudokuSolverStrategy.allCases.contains {
-                self.solve(for: $0, using: &generator) != nil
-            }
-        } while isSolved
-        
-        return self.sudoku.array.allSatisfy { $0 > 0 }
-    }
-    
-    @discardableResult
-    public mutating func solve<T>(
-        for strategy: SudokuSolverStrategy,
-        using generator: inout T
-    ) -> SudokuSolverMove? where T : RandomNumberGenerator {
-        let move: SudokuSolverMove?
-        
-        switch strategy {
-        case .fullHouse:
-            move = self.solveFullHouse(using: &generator)
-        case .nakedSingle:
-            move = self.solveNakedSingle(using: &generator)
-        case .hiddenSingle:
-            move = self.solveHiddenSingle(using: &generator)
-        case .pointingCandidate:
-            move = self.solvePointingCandidate(using: &generator)
-        case .claimingCandidate:
-            move = self.solveClaimingCandidate(using: &generator)
-        case .nakedPair:
-            move = self.solveNakedPair(using: &generator)
-        }
-        
-        guard let move else {
-            return nil
-        }
-        
-        for location in move.locations {
-            let index = location.row * 9 + location.column
-            
-            if let addedValue = location.addedValue {
-                for index in ArraySudoku.peers(location.row, location.column) {
-                    if var candidates = self.candidates[index] {
-                        candidates.remove(addedValue)
-                        self.candidates[index] = candidates
-                    }
-                }
-                
-                self.candidates.removeValue(forKey: index)
-                self.sudoku.array[index] = addedValue
-            } else {
-                if var candidates = self.candidates[index] {
-                    for candidate in location.removedCandidates {
-                        candidates.remove(candidate)
-                    }
-                    
-                    self.candidates[index] = candidates
-                }
-            }
-        }
-        
-        self.moves.append(move)
-        return move
+    public func solve<T>(using generator: inout T) -> Bool where T : RandomNumberGenerator {
+        false
     }
 }
